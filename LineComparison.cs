d8d1d9525bcd2b2934e2c41209d2731f2aed4dc2
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-07 22:01:52 +0530

    Find Line of length x,y co-ordinate and Check Two line are equal or less than or greater

--- LineComparison.cs
+++ LineComparison.cs
@@ -6,12 +6,11 @@ namespace LineComparisonProblem
 {
     class LineComparison
     {
-        public static void ISTwoLineEqual()
+        public static void CompareTwoLineEqualOrLess()
         {
             try
             {
-                double x1, y1, x2, y2;
-
+                double x1, y1, x2, y2, x3, y3, x4, y4;
                 Console.WriteLine("Enter the (x1,y1) coordinates of the first point comma separated:");
                 var x1y1 = Console.ReadLine();
                 string[] xy1 = x1y1.Split(",");
@@ -24,17 +23,36 @@ namespace LineComparisonProblem
                 x2 = Convert.ToDouble(xy2[0]);
                 y2 = Convert.ToDouble(xy2[1]);
 
-                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine("Enter the (x3,y3) coordinates of the second point comma separated:");
+                var x3y3 = Console.ReadLine();
+                string[] xy3 = x3y3.Split(",");
+                x3 = Convert.ToDouble(xy3[0]);
+                y3 = Convert.ToDouble(xy3[1]);
 
-                Console.WriteLine($"The length of the line connecting ({x1},{y1}) and ({x2},{y2}) is {length}");
-                if ((x1 == y1 && y2 == x2) || (x1 == x2 && y1 == y2))
+                Console.WriteLine("Enter the (x4,y4) coordinates of the second point comma separated:");
+                var x4y4 = Console.ReadLine();
+                string[] xy4 = x4y4.Split(",");
+                x4 = Convert.ToDouble(xy4[0]);
+                y4 = Convert.ToDouble(xy4[1]);
+
+                double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+
+                int comparisonResult = length1.CompareTo(length2);
+
+                if (comparisonResult == 0)
                 {
-                    Console.WriteLine("The two lines are equal.");
+                    Console.WriteLine("Line 1 is equal to line 2");
+                }
+                else if (comparisonResult < 0)
+                {
+                    Console.WriteLine("Line 1 is less than line 2");
                 }
                 else
                 {
-                    Console.WriteLine("The two lines are not equal.");
+                    Console.WriteLine("Line 1 is greater than line 2");
                 }
+
             }
             catch (Exception e)
             {
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
-            LineComparison.ISTwoLineEqual();
+            LineComparison.CompareTwoLineEqualOrLess();
         }
     }
 }
